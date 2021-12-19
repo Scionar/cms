4d1ddd4394cbe730751ac57b80e33d718dac6b5e
Joona Viertola <git@joonaviertola.com>
2021-12-19 22:25:44 +0200

    Add favicons

--- .eleventy.js
+++ .eleventy.js
@@ -8,30 +8,31 @@ module.exports = (eleventyConfig) => {
 
     eleventyConfig.addCollection("posts", function(collection) {
     
-        /* The posts collection includes all posts that list 'posts' in the front matter 'tags'
-           - https://www.11ty.dev/docs/collections/
-        */
-        
-        // EDIT HERE WITH THE CODE FROM THE NEXT STEPS PAGE TO REVERSE CHRONOLOGICAL ORDER
-        // (inspired by https://github.com/11ty/eleventy/issues/898#issuecomment-581738415)
-        const coll = collection
-          .getFilteredByTag("posts").sort((a, b) => b.data.date - a.data.date);
-    
-        // From: https://github.com/11ty/eleventy/issues/529#issuecomment-568257426 
-        // Adds {{ prevPost.url }} {{ prevPost.data.title }}, etc, to our njks templates
+      /* The posts collection includes all posts that list 'posts' in the front matter 'tags'
+          - https://www.11ty.dev/docs/collections/
+      */
+      
+      // EDIT HERE WITH THE CODE FROM THE NEXT STEPS PAGE TO REVERSE CHRONOLOGICAL ORDER
+      // (inspired by https://github.com/11ty/eleventy/issues/898#issuecomment-581738415)
+      const coll = collection
+        .getFilteredByTag("posts").sort((a, b) => b.data.date - a.data.date);
+  
+      // From: https://github.com/11ty/eleventy/issues/529#issuecomment-568257426 
+      // Adds {{ prevPost.url }} {{ prevPost.data.title }}, etc, to our njks templates
 
-        //   for (let i = 0; i < coll.length; i++) {
-        //     const prevPost = coll[i - 1];
-        //     const nextPost = coll[i + 1];
-      
-        //     coll[i].data["prevPost"] = prevPost;
-        //     coll[i].data["nextPost"] = nextPost;
-        //   }
-      
-           return coll;
-        });
+      //   for (let i = 0; i < coll.length; i++) {
+      //     const prevPost = coll[i - 1];
+      //     const nextPost = coll[i + 1];
+    
+      //     coll[i].data["prevPost"] = prevPost;
+      //     coll[i].data["nextPost"] = nextPost;
+      //   }
+    
+      return coll;
+    });
 
   eleventyConfig.addPassthroughCopy({'src/css': 'css'})
+  eleventyConfig.addPassthroughCopy({'src/assets': 'assets'})
     
   return {
     dir: {
